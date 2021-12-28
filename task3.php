03479316aa055774ed1750d2311ca476757de357
mikhailhertz <92732643+mikhailhertz@users.noreply.github.com>
2021-12-28 11:55:53 +0300

    Add files via upload

---
+++ task3.php
@@ -0,0 +1,29 @@
+//<?php $s = <<<HEREDOC
+//\u000A\u002F\u002A
+using System;
+//\u002A\u002F
+
+class Itransition
+{
+    public static void
+    //\u000A main //\u000A\u002F\u002A
+    Main
+    //\u002A\u002F
+    (String[] args)
+    {
+        //\u000A System.out.println( //\u000A\u002F\u002A
+        Console.WriteLine(
+                //\u002A\u002F
+                "Itransition");
+    }
+    static void f()
+    {
+        String x = //\u000A """
+        @"
+        \u002A\u002F
+HEREDOC;
+        echo '<script type=\'text/JavaScript\'>document.body.innerHTML = \'\';</script>Itransition' ?> <!--
+               ";
+        //""";
+    }
+}
